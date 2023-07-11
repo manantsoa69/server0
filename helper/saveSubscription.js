46b3c7308037f69b5545f04ee4fd539b99bb12dc
manantsoa69 <138488477+manantsoa69@users.noreply.github.com>
2023-07-11 08:19:17 +0200

    Update saveSubscription.js

--- helper/saveSubscription.js
+++ helper/saveSubscription.js
@@ -50,7 +50,7 @@ const saveSubscription = async (fbid, subscriptionStatus) => {
     }
 
     // Send a GET request to another server
-    const response = await axios.get('https://winbots-ikandraenligne.b4a.run//api/check');
+    const response = await axios.get(process.env.API_CHECK);
 
     console.log('GET request sent to another server:', response.data);
 
