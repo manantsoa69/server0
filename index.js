e0d4a96fcf867ad9e176be876ed124be3e4408b3
manantsoa69 <138488477+manantsoa69@users.noreply.github.com>
2023-07-29 07:36:05 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,125 +1,68 @@
-//index.js
-require('dotenv').config();
-const Redis = require('ioredis');
+// index.js
 const express = require('express');
-const dayjs = require('dayjs');
-const utcPlugin = require('dayjs/plugin/utc');
-const timezonePlugin = require('dayjs/plugin/timezone');
-const frLocale = require('dayjs/locale/fr');
 const responseTime = require('response-time');
-const winston = require('winston');
+const compression = require('compression'); // Import the compression middleware
 
-const { saveSubscription } = require('./helper/saveSubscription');
-const { sendMessage } = require('./helper/messengerApi');
-const { calculateExpirationDate } = require('./helper/expireDateCalculator');
 
-dayjs.extend(utcPlugin);
-dayjs.extend(timezonePlugin);
-dayjs.locale(frLocale);
-
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.simple()
-  ),
-  transports: [new winston.transports.Console()],
-});
-
+require('dotenv').config();
 const webApp = express();
 const PORT = process.env.PORT || 3000;
-
-webApp.use(express.urlencoded({ extended: true }));
-webApp.use(express.json());
-
 const startResponseTimer = (req, res, next) => {
   const start = process.hrtime();
   res.on('finish', () => {
     const end = process.hrtime(start);
     const duration = Math.round((end[0] * 1000) + (end[1] / 1000000));
-    logger.info(`Response time: ${duration}ms`);
+    console.log(`${duration}ms`);
   });
   next();
 };
 
-const logRequest = (req, res, next) => {
-  logger.info(`Path ${req.path} with Method ${req.method}`);
-  next();
-};
+// Use the compression middleware to enable Gzip compression
+webApp.use(compression());
 
+webApp.use(responseTime());
 webApp.use(startResponseTimer);
-webApp.use(logRequest);
+webApp.use(express.urlencoded({ extended: true }));
+webApp.use(express.json());
 
-const homeRoute = require('./routes/homeRoute');
+// Load the environment variables for the Production environment
+if (process.env.NODE_ENV === 'production') {
+  const numCPUs = require('os').cpus().length;
+
+  pm2.connect((err) => {
+    if (err) {
+      console.error('PM2 connection error:', err.stack || err);
+      process.exit(1);
+    }
+
+    pm2.start(
+      {
+        script: 'index.js',
+        instances: numCPUs, // Number of instances (cluster mode will use all CPU cores)
+        exec_mode: 'cluster', // Run the application in cluster mode
+        max_memory_restart: '200M', // Max memory allowed for each instance
+      },
+      (err) => {
+        if (err) {
+          console.error('PM2 start error:', err.stack || err);
+          pm2.disconnect();
+          process.exit(1);
+        }
+
+        console.log(`Server is up and running at ${PORT} using PM2 in cluster mode.`);
+        pm2.disconnect();
+      }
+    );
+  });
+}
+
+// Load other routes and middlewares
 const fbWebhookRoute = require('./routes/fbWebhookRoute');
-webApp.use('/', homeRoute.router);
+const homeRoute = require('./routes/homeRoute');
+
 webApp.use('/facebook', fbWebhookRoute.router);
-
-webApp.post('/subscribe', async (req, res, next) => {
-  try {
-    const { fbid, subscriptionStatus } = req.body;
-
-    logger.info('Received subscription request:', { fbid, subscriptionStatus });
-
-    const startDate = dayjs().utcOffset('+03:00').format('D MMMM YYYY, HH:mm:ss');
-
-    const expireDate = calculateExpirationDate(subscriptionStatus);
-
-    const success = await saveSubscription(fbid, subscriptionStatus);
-
-    if (success) {
-      logger.info('Subscription activated successfully.');
-      res.status(200).json({ message: 'Subscription activated successfully.' });
-
-      const madagascarTime = dayjs(expireDate).utcOffset('+03:00').format('D MMMM YYYY, HH:mm:ss');
-
-      const messageParts = [
-        'Félicitations ! 🎉 Votre abonnement a été activé avec succés. Nous sommes ravis de vous présenter les détails de votre souscription :',
-        `Type d'abonnement: ${subscriptionStatus}`,
-        `Date d'activation: ${startDate}`,
-        `Date d'expiration: ${madagascarTime}`,
-        `Nous espérons que vous apprécierez pleinement les avantages et les fonctionnalités offerts par votre abonnement. Si vous avez des questions ou des préoccupations, n'hésitez pas à nous contacter. Merci encore pour votre souscription et nous vous souhaitons une excellente expérience ✨!`
-      ];
-      const message = messageParts.join('\n');
-
-      await sendMessage(fbid, message);
-    } else {
-      logger.error('Failed to activate subscription.');
-      res.status(500).json({ message: 'Failed to activate subscription.' });
-    }
-  } catch (error) {
-    logger.error('Error subscribing user:', error);
-    next(error);
-  }
-});
-
-webApp.post('/send_message', async (req, res, next) => {
-  try {
-    const { fbid, message = 'Try again' } = req.body;
-
-    logger.info('Received message request:', { fbid, message });
-
-    const success = await sendMessage(fbid, message);
-
-    if (success) {
-      logger.info('Message sent successfully.');
-      res.status(200).json({ message: 'Message sent successfully.' });
-    } else {
-      logger.error('Failed to send message.');
-      res.status(500).json({ message: 'Failed to send message.' });
-    }
-  } catch (error) {
-    logger.error('Error sending message:', error);
-    next(error);
-  }
-});
-
-webApp.use((err, req, res, next) => {
-  logger.error('An error occurred:', err);
-  res.status(500).json({ error: 'Internal Server Error' });
-});
+webApp.use('/', homeRoute.router);
 
 webApp.listen(PORT, () => {
-  logger.info(`Server is up and running at ${PORT}`);
+  console.log(`Server is up and running at ${PORT}`);
 });
-
